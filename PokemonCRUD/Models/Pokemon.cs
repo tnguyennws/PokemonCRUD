33094ecab28d1219d4a66ee53f8b952db2cf8728
Timothée <tnguyen@normandiewebschool.fr>
2021-10-10 17:09:33 +0200

    Ajout de validation des champs

--- PokemonCRUD/Models/Pokemon.cs
+++ PokemonCRUD/Models/Pokemon.cs
@@ -6,10 +6,27 @@ namespace PokemonCRUD.Models
     public class Pokemon
     {
         public int Id { get; set; }
+
+        [Range(1, 1000)]
+        [DataType(DataType.Currency)]
+        [Required]
         public int IdPokedex { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
         public string Name { get; set; }
+
+        [StringLength(12, MinimumLength = 3)]
+        [Required]
         public string Type1 { get; set; }
+
+        [StringLength(12, MinimumLength = 3)]
+        [Required]
         public string Type2 { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Required]
         public int Evolution { get; set; }
     }
 }
